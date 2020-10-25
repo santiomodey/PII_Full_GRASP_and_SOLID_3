0f9b679c9f62d5425ba77b24a63440379212f3e0
Nicolas Texidor <NICOLAS.TEXIDOR@correo.ucu.edu.uy>
2020-10-25 18:27:44 -0300

    Agregados comentarios

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /// <summary>
+    /// Este es un ejemplo de la implementación de la interfaz IPrinter
+    /// donde se ve que utiliza la misma firma para la función y permite
+    /// imprimir por pantalla con su implementación
+    /// </summary>
     public class ConsolePrinter : IPrinter
     {
         public void PrintRecipe(Recipe recipe)
--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -2,6 +2,11 @@ using System.IO;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /// <summary>
+    /// Este es un ejemplo de la implementación de la interfaz IPrinter
+    /// donde se ve que utiliza la misma firma para la función y permite
+    /// imprimir en un archivo con su implementación
+    /// </summary>
     public class FilePrinter : IPrinter
     {
         public void PrintRecipe(Recipe recipe)
